11718541245bd1aad29058e0e34d5c57b182e125
Nguyen Duc Long <duclongt23@gmail.com>
2025-11-04 18:26:28 +0700

    add validation data prototype comsumer

--- ingestion/producer.py
+++ ingestion/producer.py
@@ -1,24 +1,14 @@
 import json, time, random
 from datetime import datetime, timezone
 from kafka import KafkaProducer
-import pandas as pd
+import csv
 
-producer = KafkaProducer(bootstrap_servers="kafka.default.svc.cluster.local:9092",
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda v: json.dumps(v).encode("utf-8"))
 
-texts = pd.read_csv('data/twitter_training.csv')['Text'].tolist()
+with open("twitter_validation.csv") as f:
+    reader = csv.reader(f)
+    for data in reader:
+        producer.send('test_tweet', value = data)
+        time.sleep(2)
 
-def gen_record(i):
-    return {
-        "post_id": i,
-        "user_id": random.randint(1, 10000),
-        "text": random.choice(texts),
-        "lang": "en",
-        "timestamp": datetime.now(timezone.utc).isoformat()
-    }
-
-i = 0
-while True:
-    rec = gen_record(i); i += 1
-    producer.send("posts", rec)
-    time.sleep(0.05)  # 20 msg/giây
