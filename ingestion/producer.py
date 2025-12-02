db2a21dcd9d9b576ea66d4c1bf99a3fe36cd600f
mitrahonha <minhtrhoangnhat@gmail.com>
2025-12-03 01:54:28 +0700

    modify producer

--- ingestion/producer.py
+++ ingestion/producer.py
@@ -1,14 +1,21 @@
-import json, time, random
-from datetime import datetime, timezone
+import json, time
 from kafka import KafkaProducer
 import csv
+from datetime import datetime
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda v: json.dumps(v).encode("utf-8"))
 
-with open("twitter_validation.csv") as f:
-    reader = csv.reader(f)
-    for data in reader:
-        producer.send('test_tweet', value = data)
-        time.sleep(2)
+topic = 'social_stream'
 
+with open("../data/twitter_validation.csv", encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        data  ={
+            'id': row[0],
+            'Topic': row[1],
+            'Text': row[3],
+            'event_time': datetime.now().isoformat()
+        }
+        producer.send(topic, value = data)
+        time.sleep(2)
